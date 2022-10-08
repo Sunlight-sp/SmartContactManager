eb453125ae9082b8011e929789860dc292c9b042
Sunlight-sp <sunlightchamp1@gmail.com>
2022-10-08 19:14:34 +0530

    login complete

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +24,9 @@ public class HomeController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@RequestMapping("/")
 	public String home(Model model) {
@@ -64,6 +69,7 @@ public class HomeController {
 			
 			user.setEnabled(true);
 			user.setRole("ROLE_USER");
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
 			
 			System.out.println("agreement "+ agreement);
 			System.out.println("user "+user.toString());
@@ -80,5 +86,12 @@ public class HomeController {
 			session.setAttribute("message", new Message("Something went wrong - "+e.getMessage(), "alert-danger"));
 			return "signup";
 		}
+	}
+	
+	//handler for custom login
+	@GetMapping("/signin")
+	public String customLogin(Model model) {
+		model.addAttribute("title", "LoginPage");
+		return "login";
 	}
 }
